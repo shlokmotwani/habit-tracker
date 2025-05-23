b723c00150e3308015631e9dbd107e42d7a46266
Shlok Motwani <shlok.codes@gmail.com>
2025-05-23 12:06:58 +0530

    feat: CRUD operations on User via userController

---
+++ backend/controllers/userController.js
@@ -0,0 +1,47 @@
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
+async function createUser(user) {
+  try {
+    const response = await prisma.user.create({ data: user });
+    return { message: "User created successfully.", response };
+  } catch (error) {
+    console.error("Error while creating user.", error);
+    return { message: "Error while creating user.", error };
+  }
+}
+
+async function fetchUser(email) {
+  try {
+    const user = await prisma.user.findUnique({ where: { email } });
+    if (!user) {
+      return { message: "Email address not found." };
+    }
+    return { message: "User fetched successfully.", user };
+  } catch (error) {
+    console.error("Error while fetching user.", error);
+    return { message: "Error while fetching user.", error };
+  }
+}
+
+async function updateUser(email, data) {
+  try {
+    const response = await prisma.user.update({ where: { email }, data });
+    return { message: "User updated successfully.", response };
+  } catch (error) {
+    console.error("Error while updating user.", error);
+    return { message: "Error while updating user.", error };
+  }
+}
+
+async function deleteUser(email) {
+  try {
+    await prisma.user.delete({ where: { email } });
+    return { message: "User deleted successfully." };
+  } catch (error) {
+    console.error("Error while deleting user.", error);
+    return { message: "Error while deleting user.", error };
+  }
+}
+
+export { createUser, fetchUser, updateUser, deleteUser };
